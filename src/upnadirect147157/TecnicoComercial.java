0460cf53e5bc634bd356cae5f6ad82377b860aad
pmosquemar <pmosquemar@educacion.navarra.es>
2024-12-20 00:36:56 +0100

    Inicio clase TecnicoComercial

---
+++ src/upnadirect147157/TecnicoComercial.java
@@ -0,0 +1,17 @@
+package upnadirect147157;
+
+/**
+ *
+ * @author alumno
+ */
+public class TecnicoComercial {
+    private Cliente cliente;
+    private Bien bien;
+
+    public TecnicoComercial(Cliente cliente, Bien bien) {
+        this.cliente = cliente;
+        this.bien = bien;
+    }
+    
+    
+}
