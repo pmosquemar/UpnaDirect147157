a18b8a3409626656763ad60873a9f2694b3d055b
pmosquemar <pmosquemar@educacion.navarra.es>
2024-12-21 13:16:45 +0100

    Clase Vista actualizada

--- src/upnadirect147157/Vista.java
+++ src/upnadirect147157/Vista.java
@@ -1,5 +1,6 @@
 package upnadirect147157;
 
+import java.time.LocalDate;
 import java.util.Scanner;
 
 /**
@@ -33,6 +34,16 @@ public class Vista {
         return slario;
     }
     
+    
+    public void falloCliente(){
+        System.err.println("El año de nacimiento debe estar entre 1910 y " +  LocalDate.now().getYear());
+    }
+    
+    public void falloBien(){
+        System.err.println("El tipo del bien debe ser una vivienda o un vehículo");
+        System.err.println("El valor máximo del vehículo es de 50.000€");
+        System.err.println("El valor mínimo de la vivienda es de 50.000€");
+    }
     
     
 }
