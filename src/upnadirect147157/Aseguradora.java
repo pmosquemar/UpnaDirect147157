164ba0c89b21e37ed8b5c3d99f8d041d01520450
pmosquemar <pmosquemar@educacion.navarra.es>
2024-12-21 12:14:41 +0100

    Aseguradora como interfaz

--- src/upnadirect147157/Aseguradora.java
+++ src/upnadirect147157/Aseguradora.java
@@ -4,16 +4,7 @@ package upnadirect147157;
  *
  * @author alumno
  */
-public abstract  class Aseguradora {
-    private int comision;
-    private int importe;
-    private TecnicoComercial tecnico;
-
-    public Aseguradora(int comision, int importe, TecnicoComercial tecnico) {
-        this.comision = comision;
-        this.importe = importe;
-        this.tecnico = tecnico;
-    }
+public interface Aseguradora {
     
     public abstract double calcularImporte(TecnicoComercial tecnico);
 }
